58e1a8da9b7f5f252df2ccb691837838e2052716
zhenyundeng <160323088+zhenyundeng@users.noreply.github.com>
2024-03-15 13:00:27 +0000

    Add files via upload

--- challenge_data/challenge_1/main.py
+++ challenge_data/challenge_1/main.py
@@ -1,4 +1,34 @@
-import random
+import json
+import numpy as np
+import scipy
+import nltk
+from nltk import word_tokenize
+
+
+def compute_all_pairwise_scores(src_data, tgt_data, metric):
+    X = np.empty((len(src_data), len(tgt_data)))
+
+    for i in range(len(src_data)):
+        for j in range(len(tgt_data)):
+            X[i][j] = (metric(src_data[i], tgt_data[j]))
+
+    return X
+
+
+def pairwise_metric(candidate, reference):  # Todo this is not thread safe, no idea how to make it so
+    return nltk.translate.meteor_score.single_meteor_score(word_tokenize(reference), word_tokenize(candidate))
+
+
+def compute_pairwise_evidence_score(gold_evidence, pred_evidence):
+    pairwise_scores = compute_all_pairwise_scores(pred_evidence, gold_evidence, pairwise_metric)
+    assignment = scipy.optimize.linear_sum_assignment(pairwise_scores, maximize=True)
+    assignment_utility = pairwise_scores[assignment[0], assignment[1]].sum()
+
+    # Reweight to account for unmatched target questions
+    reweight_term = 1 / float(len(gold_evidence))
+    assignment_utility *= reweight_term
+
+    return assignment_utility
 
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
@@ -42,42 +72,82 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
     """
     print(kwargs["submission_metadata"])
     output = {}
+
+    # load data
+    gold_samples = json.load(open(test_annotation_file, 'r'))
+    pred_samples = json.load(open(user_submission_file, 'r'))
+
+    assert len(gold_samples) == len(pred_samples)
+
+    # evaluate
+    averitec_reporting_levels = [0.2, 0.25, 0.3]
+    gold_labels, pred_labels, evid_scores, scores = [], [], [], []
+    for test_sample, pred_sample in zip(gold_samples, pred_samples):
+        gold_label, gold_evidence = test_sample['label'], test_sample['evidence']
+        pred_label, pred_evidence = pred_sample['label'], pred_sample['evidence']
+
+        # label
+        gold_labels.append(gold_label)
+        pred_labels.append(pred_label)
+
+        # evidence
+        score = compute_pairwise_evidence_score(gold_evidence, pred_evidence)
+        evid_scores.append(score)
+
+        # meteor
+        this_example_scores = [0.0 for _ in averitec_reporting_levels]
+        for i, level in enumerate(averitec_reporting_levels):
+            if score > level:
+                this_example_scores[i] = gold_label == pred_label
+
+        scores.append(this_example_scores)
+
+    # label accuracy
+    label_accuracy = np.mean([s == t for s, t in zip(gold_labels, pred_labels)])
+    # evidence score
+    evidence_score = np.mean(evid_scores)
+    # averitec score
+    averitec_scores = np.mean(np.array(scores), axis=0)
+
     if phase_codename == "dev":
         print("Evaluating for Dev Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                "dev_split": {
+                    "Label accuracy": label_accuracy,
+                    "Evidence score": evidence_score,
+                    "Averitec score1": averitec_scores[0],  # (meteor @ 0.2)
+                    "Averitec score2": averitec_scores[1],  # (meteor @ 0.25)
+                    "Averitec score3": averitec_scores[2],  # (meteor @ 0.3)
                 }
             }
         ]
         # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
+        output["submission_result"] = output["result"][0]["dev_split"]
         print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
+    elif phase_codename == "test" or phase_codename == "after_test":
         print("Evaluating for Test Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
                 "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                    "Label accuracy": label_accuracy,
+                    "Evidence score": evidence_score,
+                    "Averitec score1": averitec_scores[0],  # (meteor @ 0.2)
+                    "Averitec score2": averitec_scores[1],  # (meteor @ 0.25)
+                    "Averitec score3": averitec_scores[2],  # (meteor @ 0.3)
                 }
-            },
+            }
         ]
         # To display the results in the result file
-        output["submission_result"] = output["result"][0]
+        output["submission_result"] = output["result"][0]["test_split"]
         print("Completed evaluation for Test Phase")
+
     return output
+
+
+# if __name__ == "__main__":
+#     test_annotation_file = "annotations/averitec_test_gold.json"
+#     user_submission_file = "baseline_submission_test.json"
+#
+#     evaluate(test_annotation_file, user_submission_file, 'dev')
+#     print("hello")
